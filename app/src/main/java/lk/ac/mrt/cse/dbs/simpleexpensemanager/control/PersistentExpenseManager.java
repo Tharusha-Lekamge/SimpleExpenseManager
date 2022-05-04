7a687d2d35f55642f8e9042549fd801ee7016213
Tharusha Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2022-05-04 11:46:35 +0530

    Completed PersistentExpenseManager
    Added code to the PersistentExpenseManager
    
    added to main activity

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -1,22 +1,39 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
-import java.io.Serializable;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import android.content.Context;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 public class PersistentExpenseManager extends ExpenseManager{
+    private Context context;
 
+    public PersistentExpenseManager(Context context){
+        this.context = context;
+        try {
+            setup();
+        } catch (ExpenseManagerException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void setup() throws ExpenseManagerException {
+        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO(context, "190352M.db", null, 1);
+        setTransactionsDAO(persistentTransactionDAO);
 
+        AccountDAO persistentAccountDAO = new PersistentAccountDAO(context, "190352M.db", null, 1);
+        setAccountsDAO(persistentAccountDAO);
+
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
     }
 }
